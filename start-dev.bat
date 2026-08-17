1bdded3adf2e64b1c24e4b926cae2e48c18eeda5
Hunter <>
2026-08-18 04:46:03 +0500

    chore: clean up temporary python scripts and unused files

--- start-dev.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Hunterstar Next.js Server...
-echo Make sure you are accessing http://localhost:3000
-cd website
-npm run dev
--- test_pydantic.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field, ConfigDict
-class FileMetadata(BaseModel):
-    id: str = Field(alias='_id')
-doc = {'_id': '123'}
-try:
-    print(FileMetadata(**doc))
-except Exception as e:
-    print('Error:', e)
